ff43e4596f629c3835aa7d77426e49ebbcac9e08
Melissa-Castaing <melissa.castain@gmail.com>
2022-05-05 22:24:23 +0200

    component home page

--- web_site_cv/src/App.js
+++ web_site_cv/src/App.js
@@ -1,10 +1,14 @@
 import Nav from './components/Nav'
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        
       <Nav />
+      </Routes>
     </div>
   );
 }
--- web_site_cv/src/components/Nav/index.js
+++ web_site_cv/src/components/Nav/index.js
@@ -1,7 +1,12 @@
+import { NavLink } from 'react-router-dom';
 import './style.scss';
 
 export default function Nav() {
     return(
-        <div className="nav"> Je suis la nav </div>
+        <div className="nav"> 
+        <NavLink to="/" activeClassName="selected">
+            A propos
+        </NavLink>
+         </div>
     )
 }
