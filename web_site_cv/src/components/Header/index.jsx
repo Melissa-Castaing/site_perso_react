fa9ff19954a5d54c6b4ce3070599e165da66fe22
Melissa-Castaing <melissa.castain@gmail.com>
2022-05-09 16:57:55 +0200

    init and css typewriter

--- web_site_cv/src/components/Header/index.jsx
+++ web_site_cv/src/components/Header/index.jsx
@@ -1,7 +1,27 @@
+import Typewritter from 'typewriter-effect';
 import './style.scss';
 
 export default function Header() {
   return (
-    <div className="header"> Je suis le header</div>
+    <div className="header">
+      <Typewritter
+        options={{
+          autoStart: true,
+          loop: true,
+        }}
+        onInit={(typewriter) => {
+          typewriter.typeString('Je suis curieuse')
+            .pauseFor(1000)
+            .deleteChars(8);
+          typewriter.typeString('passionnée')
+            .pauseFor(1000)
+            .deleteChars(10);
+          typewriter.typeString('développeuse junior')
+            .pauseFor(3000)
+            .deleteChars(10)
+            .start();
+        }}
+      />
+    </div>
   );
 }
--- web_site_cv/src/page/HomePage.jsx
+++ web_site_cv/src/page/HomePage.jsx
@@ -1,9 +1,11 @@
 import Nav from '../components/Nav';
+import Header from '../components/Header';
 
 export default function HomePage() {
   return (
     <div>
       <Nav />
+      <Header />
     </div>
   );
 }
