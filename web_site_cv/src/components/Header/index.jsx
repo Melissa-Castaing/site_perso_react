fc7b56e08385443dcc94f41fb8a9bf203d079ad2
Melissa-Castaing <melissa.castain@gmail.com>
2022-05-09 17:35:37 +0200

    typewritter déco line done

--- web_site_cv/src/components/Header/index.jsx
+++ web_site_cv/src/components/Header/index.jsx
@@ -4,24 +4,26 @@ import './style.scss';
 export default function Header() {
   return (
     <div className="header">
+      <div className="header_line" />
       <Typewritter
         options={{
           autoStart: true,
           loop: true,
         }}
         onInit={(typewriter) => {
-          typewriter.typeString('Je suis curieuse')
-            .pauseFor(1000)
-            .deleteChars(8);
+          typewriter.typeString('Je suis curieuse, ')
+            .pauseFor(1000);
+          // .deleteChars(8);
           typewriter.typeString('passionnée')
             .pauseFor(1000)
-            .deleteChars(10);
-          typewriter.typeString('développeuse junior')
+            .deleteChars(20);
+          typewriter.typeString('développeuse full stack junior')
             .pauseFor(3000)
             .deleteChars(10)
             .start();
         }}
       />
+      <div className="header_line_under" />
     </div>
   );
 }
